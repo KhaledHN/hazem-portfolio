6f6434ba58c37a4f63ae36b914f4732b966118c0
Hazem Khaled <hazem.khaled@gmail.com>
2024-09-28 08:04:41 +0300

    chore: Update .gitignore and .vscode/settings.json files
    Signed-off-by: Hazem Khaled <hazem.khaled@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import analyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'out'
-}
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["icon-library"],
+  },
+};
 
-export default nextConfig
+const withBundleAnalyzer = analyzer({
+  enabled: process?.env?.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
